9cb1fa8dcaaadffa1bf559c4f0ab0ee385b6ba5e
HuTu-Git <hubin1015@163.com>
2022-03-18 00:39:11 +0800

    明日科技第1章

--- MingRi/Chap01/shili101/HelloJava.java
+++ MingRi/Chap01/shili101/HelloJava.java
@@ -2,6 +2,6 @@ package MingRi.Chap01.shili101;
 
 public class HelloJava{
     public static void main(String[] args) {
-        System.out.println("Hello Java");
+        System.out.println("Hello Java!");
     }
 }
--- MingRi/Chap01/xiti01/XiTi01.java
+++ MingRi/Chap01/xiti01/XiTi01.java
@@ -2,6 +2,6 @@ package MingRi.Chap01.xiti01;
 
 public class XiTi01 {
    public static void main(String[] args){
-       System.out.println("这是我的第一个Java程序");
+       System.out.println("这是我的第一个Java程序！");
    } 
 }
