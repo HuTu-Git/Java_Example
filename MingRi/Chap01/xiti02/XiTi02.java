acb77947d7810fc91d207e55a981859abf6e8d47
HuTu-Git <hubin1015@163.com>
2022-03-17 23:04:28 +0800

    明日科技第1章

--- MingRi/Chap01/xiti02/XiTi02.java
+++ MingRi/Chap01/xiti02/XiTi02.java
@@ -2,7 +2,7 @@ package MingRi.Chap01.xiti02;
 
 public class XiTi02 {
     public static void main(String[] args) {
-        System.out.println("-----------");
+        System.out.println("------------");
         System.out.println("| 我要学会 |");
         System.out.println("| JAVA语言 |");
         System.out.println("------------");
